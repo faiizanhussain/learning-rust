81089bc3a3038cde7abe1acb35264461d07dba41
faiizanhussain <faiizanhussain@gmail.com>
2024-06-18 17:13:16 +0500

    inside commit

--- exercises/07_structs/structs3.rs
+++ exercises/07_structs/structs3.rs
@@ -31,12 +31,18 @@ impl Package {
         }
     }
 
-    fn is_international(&self) -> ??? {
-        // Something goes here...
+    fn is_international(&self) -> bool {
+        
+       let checkIt = if self.sender_country == self.recipient_country {false} else {true};
+
+       return checkIt; 
+
     }
 
-    fn get_fees(&self, cents_per_gram: u32) -> ??? {
-        // Something goes here...
+    fn get_fees(&self, cents_per_gram: u32) -> u32 {
+        let fees = cents_per_gram * self.weight_in_grams;
+        fees
+
     }
 }
 
