7fe29bab420937e46fd47be7cd862c4fd927b72d
faiizanhussain <faiizanhussain@gmail.com>
2024-06-22 13:44:37 +0500

    Tests, Done

--- exercises/17_tests/tests3.rs
+++ exercises/17_tests/tests3.rs
@@ -7,7 +7,6 @@
 // Execute `rustlings hint tests3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 pub fn is_even(num: i32) -> bool {
     num % 2 == 0
@@ -19,11 +18,15 @@ mod tests {
 
     #[test]
     fn is_true_when_even() {
-        assert!();
+        let num = 2;
+        assert!(is_even(num), "2 is even");
+        // assert!();
     }
 
     #[test]
     fn is_false_when_odd() {
-        assert!();
+        let num = 5;
+        assert!(!is_even(num), "5 is not odd");
+        // assert!();
     }
 }
